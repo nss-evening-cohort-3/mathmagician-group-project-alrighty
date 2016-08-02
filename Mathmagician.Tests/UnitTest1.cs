6d4fe35fa4e53d3916035b8fdc469e37349942e9
abbie <abigail.c.holder@gmail.com>
2016-08-01 21:17:17 -0500

    initial commit & integer creation

--- Mathmagician.Tests/UnitTest1.cs
+++ Mathmagician.Tests/UnitTest1.cs
@@ -3,12 +3,29 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Mathmagician.Tests
 {
-    [TestClass]
+    [TestClass] // <- annotation; series of code that applies additional functionality to whatever it's attached to
     public class UnitTest1
     {
         [TestMethod]
         public void TestMethod1()
         {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void ThisShouldFail()
+        {
+            // Arrange
+
+            // Act 
+
+            // Assert
+            Assert.Fail();
         }
     }
 }
