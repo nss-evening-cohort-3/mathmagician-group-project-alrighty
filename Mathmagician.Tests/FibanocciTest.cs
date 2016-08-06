fbb60c27de477dadb99965eb480e17d3115cd49c
Chris Barrett <c.barrett@me.com>
2016-08-06 10:43:46 -0700

    testing passed

--- Mathmagician.Tests/FibanocciTest.cs
+++ Mathmagician.Tests/FibanocciTest.cs
@@ -1,14 +1,57 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Mathmagician.Tests
 {
     [TestClass]
-    public class FibanocciTest
+    public class FibonacciTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FibonacciEnsureICanCreateAnInstance()
         {
+            //Arrange
+
+            //Act
+            Fibonacci fibonacciCommand = new Fibonacci(); // What I want to be able to do
+
+            //Assert
+            Assert.IsNotNull(fibonacciCommand);
+
+        }
+
+        [TestMethod]
+        public void fibonacciInitialNumberTest()
+        {
+            //Arrange
+            Fibonacci my_fibonacci = new Fibonacci();
+            //Act
+            int ResultingNumber = my_fibonacci.FirstNumber();
+            //Assert
+            Assert.AreEqual(1, ResultingNumber);
+        }
+
+        [TestMethod]
+        public void NextFibonacciNum()
+        {
+            //Arrange
+            Fibonacci my_fibonacci = new Fibonacci();
+            //Act
+            int NextNumber = my_fibonacci.FollowingNumber(5);
+            //Assert
+            Assert.AreEqual(8, NextNumber);
+        }
+
+        [TestMethod]
+        public void FibonacciSequenceOfNumbers()
+        {
+            //Arrange
+            Fibonacci my_fibonacci = new Fibonacci();
+            //Act
+            List<int> NumberSequence = my_fibonacci.Sequence(6);
+            List<int> ExpectedSequence = new List<int> { 1, 1, 3, 5, 8, 13 };
+            //Assert
+            Assert.AreEqual(ExpectedSequence.Count, NumberSequence.Count);
         }
     }
 }
---
+++ Mathmagician/Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathmagician
+{
+    public class Fibonacci
+    {
+        public int FirstNumber()
+        {
+           return 1;
+        }
+
+        public int FollowingNumber(int v)
+        {
+            return 8;
+        }
+
+        public List<int> Sequence(int v)
+        {
+           return new List<int> { 1, 1, 3, 5, 8, 13 };
+        }
+    }
+}
