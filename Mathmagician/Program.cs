c287cb59e2cf21a19a4afab38b9486409b8e4fb5
Chris Barrett <c.barrett@me.com>
2016-08-08 18:23:52 -0700

    Wrote out user interface

--- Mathmagician/Program.cs
+++ Mathmagician/Program.cs
@@ -10,6 +10,50 @@ namespace Mathmagician
     {
         static void Main(string[] args)
         {
+
+            string prompt = "> ";
+
+            Console.WriteLine("What number pattern would you like for me to use?");
+            Console.WriteLine();
+            Console.WriteLine("Integer  |   Prime   |   Fibonacci   |   Even    |   Odd");
+            Console.WriteLine();
+            Console.Write(prompt);
+            string command = Console.ReadLine().ToLower();
+            Console.WriteLine();
+            Console.WriteLine("How many should I print?");
+            Console.WriteLine();
+            int user_number = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Cool. I'm going to print " + user_number + " " + command + " numbers.");
+            Console.WriteLine();
+
+            if (command == "odd")
+            {
+                for (var i = 1; i < user_number * 2; i++)
+                {
+                    Console.Write(i++ + " ");
+                }
+            } else if (command == "even")
+            {
+                for (var i = 2; i < user_number * 2; i++)
+                {
+                    Console.Write(i++ + " ");
+                }
+            } else if (command == "prime")
+            {
+                for (var i = 1; i < user_number; i++)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            
+
+            Console.ReadKey();
+
+
+
         }
+
     }
 }
