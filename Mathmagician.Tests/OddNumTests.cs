9455b25bbd5dc55fa3d8bec0a51208c66c5fd35f
Chris Barrett <c.barrett@me.com>
2016-08-13 08:35:09 -0700

    fixed errors

--- Mathmagician.Tests/OddNumTests.cs
+++ Mathmagician.Tests/OddNumTests.cs
@@ -21,35 +21,12 @@ namespace Mathmagician.Tests
         }
 
         [TestMethod]
-        public void OddInitialNumberTest()
-        {
-            //Arrange
-            OddNum my_odd = new OddNum();
-            //Act
-            int ResultingNumber = my_odd.FirstNumber();
-            //Assert
-            Assert.AreEqual(1, ResultingNumber);
-        }
-
-        [TestMethod]
-        public void NextOddNum()
-        {
-            //Arrange
-            OddNum my_odd = new OddNum();
-            //Act
-            int NextNumber = my_odd.FollowingNumber(3);
-            //Assert
-            Assert.AreEqual(5, NextNumber);
-
-        }
-
-        [TestMethod]
         public void OddSequenceOfNumbers()
         {
             //Arrange
             OddNum my_odd = new OddNum();
             //Act
-            List<int> NumberSequence = my_odd.Sequence(6);
+            List<int> NumberSequence = my_odd.OddSequence(6);
             List<int> ExpectedSequence = new List<int> { 1, 3, 5, 7, 9, 11 };
             //Assert
             Assert.AreEqual(ExpectedSequence.Count, NumberSequence.Count);
