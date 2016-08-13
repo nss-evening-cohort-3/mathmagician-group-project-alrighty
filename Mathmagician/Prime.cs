10a703ffc946e52e61dfa75c4bffb2906c501843
abbie <abigail.c.holder@gmail.com>
2016-08-13 09:54:37 -0500

    possibly awesome prime method, possibly worthless prime method

--- Mathmagician/Prime.cs
+++ Mathmagician/Prime.cs
@@ -18,7 +18,7 @@ namespace Mathmagician
             return 3;
         }
 
-        public static bool isPrime(int increasingNumber)
+ /*       public static bool isPrime(int increasingNumber)
         {
             int boundary = (int)Math.Floor(Math.Sqrt(increasingNumber));
             if (increasingNumber == 1)
@@ -33,20 +33,35 @@ namespace Mathmagician
 
             return true;
         }
-
+        */
         public List<int> PrimeSequence(int user_number)
         {
             return new List<int> { 2, 3, 5, 7, 11, 13 };
 
             //for loop that takes user input and places however many ints the user wants into a list
             List<int> PrimeOutputList = new List<int>();
+            PrimeOutputList.Add(2);
+            int followingPrime = 3;
 
-            //INCORRECT, NEED TO MERGE THE TEST FOR PRIMES ^^^ WITH THE LIST ADDING
-            for (int i = 2; i <= user_number; i++)
+            // THE FOLLOWING ONLY TESTS ODD NUMBERS 
+            while (PrimeOutputList.Count < user_number)
             {
-                PrimeOutputList.Add(i);
+                int sqrt = (int)Math.Sqrt(followingPrime);
+                bool isPrime = true;
+                for (int i = 0; (int)PrimeOutputList[i] <= sqrt; i++)
+                {
+                    if (followingPrime % PrimeOutputList[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    PrimeOutputList.Add(followingPrime);
+                }
+                followingPrime += 2;
             }
-
             return PrimeOutputList;
         }
     }
