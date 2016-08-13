d4d1b198692beb2314591da8afa6aa7e4851d228
abbie <abigail.c.holder@gmail.com>
2016-08-12 22:24:46 -0500

    tests for prime but doesn't add to list

--- Mathmagician/Prime.cs
+++ Mathmagician/Prime.cs
@@ -18,16 +18,16 @@ namespace Mathmagician
             return 3;
         }
 
-        public static bool isPrime(int number)
+        public static bool isPrime(int increasingNumber)
         {
-            int boundary = (int)Math.Floor(Math.Sqrt(number));
-            if (number == 1)
+            int boundary = (int)Math.Floor(Math.Sqrt(increasingNumber));
+            if (increasingNumber == 1)
                 return false;
-            if (number == 2)
+            if (increasingNumber == 2)
                 return false;
             for (int i = 2; i <= boundary; i++)
             {
-                if (number % i == 0)
+                if (increasingNumber % i == 0)
                     return false;
             }
 
@@ -40,10 +40,14 @@ namespace Mathmagician
 
             //for loop that takes user input and places however many ints the user wants into a list
             List<int> PrimeOutputList = new List<int>();
-            for (int i == 2; i <= user_number; i++)
-            {
 
+            //INCORRECT, NEED TO MERGE THE TEST FOR PRIMES ^^^ WITH THE LIST ADDING
+            for (int i = 2; i <= user_number; i++)
+            {
+                PrimeOutputList.Add(i);
             }
+
+            return PrimeOutputList;
         }
     }
 }
